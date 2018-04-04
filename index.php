8cf352dd1cb4c74567507c3f44738ef1e1497331
Daniela Munoz <daniela.munoz@enova.mx>
2018-04-04 18:37:21 -0500

    Adding real content to slider to make it work as expected
    - Modifying slider css styles
    - Adding the slider real content and images

--- index.php
+++ index.php
@@ -7,8 +7,37 @@ get_header(); ?>
 <!-- Escupe la ruta hacia la carpeta raiz del templete <?php echo get_template_directory_uri(); ?> -->
 
 <div class="owl-carousel">
-	<div class="item one"> 1 </div>
-	<div class="item two"> 2 </div>
-	<div class="item"> 3 </div>
+	<div class="slide one">
+		<div class="slide-info">
+			<div class="main-width">
+				<h2>Inicio</h2>
+				<p>Expertos en consultoría sobre Asuntos Regulatorios y Sistemas de Calidad para la industria:</p>
+				<ul>
+					<li>Farmacéutica</li>
+					<li>Farmoquímica</li>
+					<li>Dispositivos médicos</li>
+					<li>Alimentaria</li>
+					<li>Cosmética</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="slide two">
+		<div class="slide-info">
+			<div class="main-width">
+				<h2>Inicio</h2>
+				<p>Alcance a:</p>
+				<ul>
+					<li>Desarrollo e implementación de la estrategia regulatoria para la obtención de autorizaciones(Licencias Sanitarias, CBPF, PSI, etc.) ante COFEPRIS.</li>
+					<li>Desarrollo e implementación de estrategia regulatoria para obtener la aprobación de FDA(Food and Drug Administration), HPB(Health Protection Branch), EMA(European Medicines Agency) y agencias sanitarias Latinoamericanas.</li>
+					<li>Auditorías internas y a proveedores</li>
+					<li>Capacitación a la medida</li>
+					<li>Reclutamiento de personal y/o outsourcing.</li>
+					<li>Servicios de validación y calibración</li>
+					<li>Trámites ante COFEPRIS</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
 <?php get_footer(); ?>
