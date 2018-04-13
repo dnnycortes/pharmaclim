f629438396b023b3159e2270141f3804d0225b54
Dany Cortes <daniela.munozc@gmail.com>
2018-04-13 17:23:17 -0500

    Adding clients logo into template

--- page-clientes.php
+++ page-clientes.php
@@ -19,23 +19,25 @@ get_header(); ?>
 			<h2>Clientes</h2>
 			<p>Atendemos a clientes nacionales e internacionales de las industrias mencionadas.</p>
 			<div class="logos">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-amazon.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-basf.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-heneiken.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-holcim.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-linkedin.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/01.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/02.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/03.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/04.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/05.png" alt="">
 
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-amazon.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-basf.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-heneiken.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-holcim.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-linkedin.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/06.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/07.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/08.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/09.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/10.png" alt="">
 
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-amazon.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-basf.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-heneiken.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-holcim.png" alt="">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/logo-linkedin.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/11.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/12.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/13.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/14.png" alt="">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/15.png" alt="">
+
+				<img src="<?php echo get_template_directory_uri(); ?>/images/clientes/16.png" alt="">
 			</div>
 		</div><!-- .main-width -->
 		<?php get_footer(); ?>
