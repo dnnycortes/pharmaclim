3cf4d8d6c957343fc909f3eec9ef01509956cade
Dany Cortes <daniela.munozc@gmail.com>
2018-04-13 17:56:28 -0500

    Removing owl slider and adding parallax effect in home template

--- index.php
+++ index.php
@@ -6,22 +6,42 @@
 get_header(); ?>
 <!-- Escupe la ruta hacia la carpeta raiz del templete <?php echo get_template_directory_uri(); ?> -->
 
-<div class="owl-carousel">
-	<div class="slide one">
-		<div class="slide-info">
-			<div class="main-width">
-				<h2>Inicio</h2>
-				<p>Expertos en consultoría sobre Asuntos Regulatorios y Sistemas de Calidad para la industria:</p>
-				<ul>
-					<li>Farmacéutica</li>
-					<li>Farmoquímica</li>
-					<li>Dispositivos médicos</li>
-					<li>Alimentaria</li>
-					<li>Cosmética</li>
-				</ul>
-			</div>
+<div class="slide one">
+	<div class="slide-info">
+		<div class="main-width">
+			<h2>Inicio</h2>
+			<p>Expertos en consultoría sobre Asuntos Regulatorios y Sistemas de Calidad para la industria:</p>
+			<ul>
+				<li>Farmacéutica</li>
+				<li>Farmoquímica</li>
+				<li>Dispositivos médicos</li>
+				<li>Alimentaria</li>
+				<li>Cosmética</li>
+			</ul>
+		</div><!-- .main-width -->
+	</div><!-- .slide-info -->
+</div><!-- .slide -->
+
+<div class="slide two">
+	<div class="slide-info">
+		<div class="main-width">
+			<h2>Inicio</h2>
+			<p>Alcance a:</p>
+			<ul>
+				<li>Desarrollo e implementación de la estrategia regulatoria para la obtención de autorizaciones(Licencias Sanitarias, CBPF, PSI, etc.) ante COFEPRIS.</li>
+				<li>Desarrollo e implementación de estrategia regulatoria para obtener la aprobación de FDA(Food and Drug Administration), HPB(Health Protection Branch), EMA(European Medicines Agency) y agencias sanitarias Latinoamericanas.</li>
+				<li>Auditorías internas y a proveedores</li>
+				<li>Capacitación a la medida</li>
+				<li>Reclutamiento de personal y/o outsourcing.</li>
+				<li>Servicios de validación y calibración</li>
+				<li>Trámites ante COFEPRIS</li>
+			</ul>
 		</div>
-	</div>
+	</div><!-- .slide-info -->
+</div><!-- .slide -->
+
+<!--
+<div class="owl-carousel">
 	<div class="slide two">
 		<div class="slide-info">
 			<div class="main-width">
@@ -40,4 +60,5 @@ get_header(); ?>
 		</div>
 	</div>
 </div>
+-->
 <?php get_footer(); ?>
