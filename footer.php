922f456a07ebc963a55590eb2fbe487bfdfd4660
dnnycortes <daniela.munozc@gmail.com>
2019-11-13 20:56:27 -0600

    Feature | Adding footer quote on english version

--- footer.php
+++ footer.php
@@ -7,10 +7,18 @@
 
 <footer class="main-footer">
 	<div class="main-width">
-		<div class="quote">
-			<p>"Aquello que para la oruga es el fin del mundo, <br> para el resto del mundo se llama mariposa</p>
-			<span>Lao Tse</span>
-		</div>
+		<?php $currentlang = get_bloginfo('language');
+		if($currentlang=="es-ES"):?>
+			<div class="quote">
+				<p>"Aquello que para la oruga es el fin del mundo, <br> para el resto del mundo se llama mariposa"</p>
+				<span>Lao Tse</span>
+			</div>
+		<?php else: ?>
+			<div class="quote">
+				<p>“What the caterpillar calls the end of the world, <br> for the rest of the world calls the butterfly”</p>
+				<span>Lao Tse</span>
+			</div>
+		<?php endif; ?>
 	</div>
 </footer>
 <footer class="sec-footer">
